d02915134761af53a019bf328973d8d5c1df6c44
6heLL66 <sseniknik@gmail.com>
2023-06-11 22:12:20 +0300

    fix: fix markup

--- src/widgets/header/ui.tsx
+++ src/widgets/header/ui.tsx
@@ -52,88 +52,102 @@ export const AppHeader: React.FC = () => {
       >
         <Toolbar
           sx={{
+            display: 'flex',
             justifyContent: 'space-between',
             padding: '20px 60px',
           }}
         >
-          <Link to='/'>
-            <LogoWhite />
-          </Link>
-
-          <MenuContainer>
-            <Link to='/claim-badge'>
-              <ClaimBadgeButton isActive={pathname === '/claim-badge'} />
+          <Box
+            sx={{
+              minWidth: '20%',
+            }}
+          >
+            <Link to='/'>
+              <LogoWhite />
             </Link>
+          </Box>
+          <Box sx={{ display: 'flex', gap: '32px', alignItems: 'center' }}>
+            <MenuContainer>
+              <Link to='/claim-badge'>
+                <ClaimBadgeButton isActive={pathname === '/claim-badge'} />
+              </Link>
 
-            <MenuLink link='/games' label='Games' pathname={pathname} />
-            <Dropdown
-              title={<Typography variant='links'>Marketplace</Typography>}
-              offsetLeft='110px'
-              offsetTop='60px'
-              color={
-                pathname.split('/').includes('marketplace')
-                  ? 'primary'
-                  : 'tertiary'
-              }
-              sx={{ padding: '10px 16px', borderRadius: '10px' }}
-              width='200px'
-              noArrow
-              items={[
-                <Link to='/marketplace/community'>
-                  <DropdownItem
-                    leftIcon={<CommunityMarket width={24} height={24} />}
-                    sx={{ padding: '8px' }}
-                  >
-                    Community
-                  </DropdownItem>
-                </Link>,
-                <Link to='/marketplace/guild'>
-                  <DropdownItem
-                    leftIcon={<GuildMarket width={24} height={24} />}
-                    sx={{ padding: '8px' }}
-                  >
-                    Guild
-                  </DropdownItem>
-                </Link>,
-              ]}
-            />
-            <MenuLink link='/blog' label='Blog' pathname={pathname} />
-            <MenuLink link='/docs' label='Docs' pathname={pathname} />
-          </MenuContainer>
-
-          {isConnected && <Button variant='contained'>Rent NFTs</Button>}
-
-          {isConnected && (
-            <RightPanel>
-              <Button
-                variant='contained'
-                color='tertiary'
-                sx={{ cursor: 'default', padding: '12px 12px', minWidth: 0 }}
-              >
-                {WalletIcon && <WalletIcon width={24} height={24} />}
-              </Button>
-              <Profile>
-                <Dropdown
-                  title='Profile'
-                  offsetLeft='52px'
-                  width='240px'
-                  items={profileDropdownItems}
-                />
-                <Avatar>
-                  <ProfilePicture account_lvl={AccountLvl._4} />
-                </Avatar>
-              </Profile>
-            </RightPanel>
-          )}
+              <MenuLink link='/games' label='Games' pathname={pathname} />
+              <Dropdown
+                title={<Typography variant='links'>Marketplace</Typography>}
+                offsetLeft='110px'
+                offsetTop='60px'
+                color={
+                  pathname.split('/').includes('marketplace')
+                    ? 'primary'
+                    : 'tertiary'
+                }
+                sx={{ padding: '10px 16px', borderRadius: '10px' }}
+                width='200px'
+                noArrow
+                items={[
+                  <Link to='/marketplace/community'>
+                    <DropdownItem
+                      leftIcon={<CommunityMarket width={24} height={24} />}
+                      sx={{ padding: '8px' }}
+                    >
+                      Community
+                    </DropdownItem>
+                  </Link>,
+                  <Link to='/marketplace/guild'>
+                    <DropdownItem
+                      leftIcon={<GuildMarket width={24} height={24} />}
+                      sx={{ padding: '8px' }}
+                    >
+                      Guild
+                    </DropdownItem>
+                  </Link>,
+                ]}
+              />
+              <MenuLink link='/blog' label='Blog' pathname={pathname} />
+              <MenuLink link='/docs' label='Docs' pathname={pathname} />
+            </MenuContainer>
+            {isConnected && <Button variant='contained'>Rent NFTs</Button>}
+            {isConnected && (
+              <RightPanel>
+                <Button
+                  variant='contained'
+                  color='tertiary'
+                  sx={{ cursor: 'default', padding: '12px 12px', minWidth: 0 }}
+                >
+                  {WalletIcon && <WalletIcon width={24} height={24} />}
+                </Button>
+                <Profile>
+                  <Dropdown
+                    title='Profile'
+                    offsetLeft='52px'
+                    width='240px'
+                    items={profileDropdownItems}
+                  />
+                  <Avatar>
+                    <ProfilePicture account_lvl={AccountLvl._4} />
+                  </Avatar>
+                </Profile>
+              </RightPanel>
+            )}
+          </Box>
 
           {!isConnected && (
-            <Button
-              variant='contained'
-              size='medium'
-              onClick={() => openModal(MODAL_NAMES.SELECT_WALLET)}
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'flex-end',
+                minWidth: '256px',
+              }}
             >
-              Connect wallet
-            </Button>
+              <Button
+                variant='contained'
+                size='medium'
+                onClick={() => openModal(MODAL_NAMES.SELECT_WALLET)}
+              >
+                Connect wallet
+              </Button>
+            </Box>
           )}
         </Toolbar>
       </AppBar>
