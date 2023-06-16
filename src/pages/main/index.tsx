4fe02499ecdce8f2110d59f17396854f7daae0d8
6heLL66 <sseniknik@gmail.com>
2023-06-17 00:47:36 +0300

    fix: move folders

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,12 +1,12 @@
 import { Typography } from '@mui/material'
 import { WalletContext } from 'app/contexts/wallet'
+import { HowItWorks } from 'pages/main/ui/how_it_works'
+import { MainBenefits } from 'pages/main/ui/main_benefits'
+import { RentAndEarn } from 'pages/main/ui/rent_and_earn'
+import { TopStrategies } from 'pages/main/ui/top_strategies'
 import { useContext } from 'react'
 import rent_bg from 'shared/assets/images/main_rent.png'
-import { HowItWorks } from 'widgets/how_it_works'
 import { JoinDiscord } from 'widgets/join_discord'
-import { MainBenefits } from 'widgets/main_benefits'
-import { RentAndEarn } from 'widgets/rent_and_earn'
-import { TopStrategies } from 'widgets/top_strategies'
 
 import { Benefit, Container, RentAndEarnContainer } from './style'
 
