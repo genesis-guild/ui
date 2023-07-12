c74ae7f59cfd6c3d473754035d8368c47cecddbf
6heLL66 <sseniknik@gmail.com>
2023-07-12 18:00:10 +0300

    feat: add sign in and link wallet modals

---
+++ src/shared/components/copy_tag/ui.tsx
@@ -0,0 +1,47 @@
+import { Box, Tooltip, Typography, useTheme } from '@mui/material'
+import useCopy from 'use-copy'
+
+import { BorderButton } from '../border_button'
+
+interface Props {
+  text: string
+  maxSymbols?: number
+}
+
+const END_SYMBOLS_COUNT = 3
+
+export const CopyTag: React.FC<Props> = ({ text, maxSymbols = 8 }) => {
+  const theme = useTheme()
+  const [copied, copy, setCopied] = useCopy(text)
+
+  const handleClick = (): void => {
+    copy()
+
+    setTimeout(() => {
+      setCopied(false)
+    }, 1000)
+  }
+
+  return (
+    <Box component='span' sx={{ display: 'inline-block' }}>
+      <Tooltip title='Copied!' open={copied} placement='top'>
+        <BorderButton
+          onClick={handleClick}
+          padding='4px 8px 4px 12px'
+          sx={{ width: 'fit-content' }}
+          background={theme.custom.colors.surface2.enable}
+          hover={theme.custom.colors.surface2.hover}
+          borderRadius='10px'
+        >
+          <Typography variant='caption2'>{`${text.slice(
+            0,
+            maxSymbols - END_SYMBOLS_COUNT,
+          )}...${text.slice(
+            text.length - END_SYMBOLS_COUNT,
+            text.length,
+          )}`}</Typography>
+        </BorderButton>
+      </Tooltip>
+    </Box>
+  )
+}
